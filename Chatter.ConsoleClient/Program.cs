bd142540fdc90f03e496214d4bf0e7bd0e016a00
shyam <shyammistry96@gmail.com>
2021-03-16 20:03:05 +0000

    feat: add base implementation for console client

---
+++ Chatter.ConsoleClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Chatter.ConsoleClient
+{
+    class Program
+    {
+        
+        static async Task Main(string[] args)
+        {
+            HubConnection _connection =  new HubConnectionBuilder()
+                .WithUrl("https://localhost:44359/MessageHub")
+                .Build();
+
+            _connection.Closed += async (error) =>
+            {
+                Console.WriteLine("CorBlimey");
+                await _connection.StartAsync();
+            };
+            await _connection.StartAsync();
+
+        }
+    }
+}
