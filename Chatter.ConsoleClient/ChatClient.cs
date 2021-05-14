8ae6972656187ad20248457d8155ed7a6c4c545b
shyam <shyammistry96@gmail.com>
2021-05-14 21:05:26 +0100

    refactor: extract chatclient to own file

---
+++ Chatter.ConsoleClient/ChatClient.cs
@@ -0,0 +1,52 @@
+﻿using Chatter.BusinessLogic.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Chatter.ConsoleClient
+{
+    public class ChatClient : IObserver<Message>
+    {
+        private readonly IHubConnectionProvider _hubConnectionProvider;
+        private readonly IMessageService _messageService;
+
+        public ChatClient(IHubConnectionProvider hubConnectionProvider, IMessageService messageService)
+        {
+            _hubConnectionProvider = hubConnectionProvider;
+            _messageService = messageService;
+            _hubConnectionProvider.Subscribe(this);
+
+        }
+        public async Task JoinGroup(int groupId)
+        {
+            await _hubConnectionProvider.JoinGroup(groupId);
+        }
+
+        public void OnCompleted()
+        {
+            Console.WriteLine("completed?");
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new Exception(error.Message);
+        }
+
+        public void OnNext(Message message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public async Task SendMessage(string messageText, int groupId)
+        {
+            var message = new Message()
+            {
+                Text = messageText,
+                SentBy = "Console",
+                GroupId = groupId,
+                SentDate = DateTime.Now
+            };
+            await _messageService.SendMessage(message).ConfigureAwait(false);
+        }
+        
+    }
+}
--- Chatter.ConsoleClient/Program.cs
+++ Chatter.ConsoleClient/Program.cs
@@ -1,5 +1,4 @@
-﻿using Chatter.BusinessLogic.Models;
-using Microsoft.AspNetCore.SignalR.Client;
+﻿using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
@@ -47,50 +46,5 @@ namespace Chatter.ConsoleClient
             }
         }
 
-    }
-    public class ChatClient : IObserver<Message>
-    {
-        private readonly IHubConnectionProvider _hubConnectionProvider;
-        private readonly IMessageService _messageService;
-
-        public ChatClient(IHubConnectionProvider hubConnectionProvider, IMessageService messageService)
-        {
-            _hubConnectionProvider = hubConnectionProvider;
-            _messageService = messageService;
-            _hubConnectionProvider.Subscribe(this);
-
-        }
-        public async Task JoinGroup(int groupId)
-        {
-            await _hubConnectionProvider.JoinGroup(groupId);
-        }
-
-        public void OnCompleted()
-        {
-            Console.WriteLine("completed?");
-        }
-
-        public void OnError(Exception error)
-        {
-            throw new Exception(error.Message);
-        }
-
-        public void OnNext(Message message)
-        {
-            Console.WriteLine(message);
-        }
-
-        public async Task SendMessage(string messageText, int groupId)
-        {
-            var message = new Message()
-            {
-                Text = messageText,
-                SentBy = "Console",
-                GroupId = groupId,
-                SentDate = DateTime.Now
-            };
-            await _messageService.SendMessage(message).ConfigureAwait(false);
-        }
-        
     }
 }
